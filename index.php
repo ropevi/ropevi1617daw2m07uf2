9c1e94ea898bdc5b24f8645192f85da6f02fd7c2
ropevi <pedrooos.46@gmail.com>
2017-03-21 18:00:48 +0100

    First commit examen

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<HTML>
+<HEAD>
+	<meta content="text/html; charset=UTF-8" http-equiv="content-type">
+	<TITLE>EXAMEN M07UF2 DAW2 2016-2017</TITLE>
+</HEAD>
+<BODY>
+    <?php
+        setcookie("desenvolupador","ropevi1617daw2",time()+3600,"/","",0);
+    ?>
+    <H3>Escriviu el nombre de temperatures a introduir : </H3>
+	<FORM method='post' action='m07uf2a.php'>
+		<input name="nombreDeTemperatures" value="3"><BR>
+		<INPUT type ='submit' value='Tramet'>
+    </FORM>
+</BODY>
+</HTML>	
